872f53cb84b8cad6cfb210c1b8b04119f4accbad
Yi Chun <b03704074@ntu.edu.tw>
2019-10-12 18:27:22 +0800

    jest testing for subjects, errors, axios

--- server/model/subject.js
+++ server/model/subject.js
@@ -5,14 +5,16 @@ const {Subject} = require('../lib/schema.js');
 
 module.exports={
     // ! if add new subject, cache should be clear manually
-    readAll: (id) => {
+    readAll: () => {
         return bookshelf.transaction( async (transaction) => {
             return cache.getAsync('subjects').then(async (cacheSubjects) => {
-                if (cacheSubjects) 
+                if (cacheSubjects) {
                     return JSON.parse(cacheSubjects);
+                }
                 let subject = await Subject.fetchAll({require:false});
                 if (subject) {
                     cache.set('subjects', JSON.stringify(subject));
+                    console.log(subject.toJSON());
                     return (subject.toJSON());
                 }
                 else {
